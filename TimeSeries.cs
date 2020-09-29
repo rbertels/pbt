362764c9602e855c783674fcb4d49ab5e9ad8696
Rob Bertels <bertels.rob@gmail.com>
2020-09-29 21:16:16 +0200

    Converted timeseries to decimals to fix rounding errors

--- TimeSeries.cs
+++ TimeSeries.cs
@@ -1,4 +1,5 @@
 using System;
+using FsCheck;
 using FsCheck.Xunit;
 using Xunit;
 
@@ -6,15 +7,15 @@ namespace pbt
 {
     public struct TimeSeriesEntries
     {
-        public float Minimum { get; set; }
+        public decimal Minimum { get; set; }
 
-        public float Maximum { get; set; }
+        public decimal Maximum { get; set; }
 
         public int Amount { get; set; }
 
-        public double Sum { get; set; }
+        public decimal Sum { get; set; } 
 
-        public static TimeSeriesEntries FromMeasurement(float value)
+        public static TimeSeriesEntries FromMeasurement(decimal value)
         {
             return new TimeSeriesEntries()
             {
@@ -41,8 +42,8 @@ namespace pbt
             return new TimeSeriesEntries()
             {
                 Amount = 0,
-                Maximum = float.NegativeInfinity,
-                Minimum = float.PositiveInfinity,
+                Maximum = decimal.MinValue,
+                Minimum = decimal.MaxValue,
                 Sum = 0
             };
         }
@@ -51,19 +52,39 @@ namespace pbt
 
     public class TimeSeriesTests
     {
-        [Property]
-        public void TestCombineWithIdentityChangesNothing(float value)
+        class TemperatureMeasurementGenerator
+        {
+            // A custom generator to make sure we dont get rounding errors
+            public static Arbitrary<decimal> Decimal()
+            {
+                return Arb.Default.Int32().Generator.Select(x => (decimal)x / 100.0m).ToArbitrary();
+            }
+        }
+
+        [Property(Arbitrary = new[] { typeof(TemperatureMeasurementGenerator) })]
+        public void TestCombineWithIdentityChangesNothing(decimal value)
         {
             var ts = TimeSeriesEntries.FromMeasurement(value);
-            
+
             var r1 = TimeSeriesEntries.Combine(ts, TimeSeriesEntries.Identity());
             var r2 = TimeSeriesEntries.Combine(TimeSeriesEntries.Identity(), ts);
 
             Assert.Equal(ts, r1);
             Assert.Equal(ts, r2);
         }
+
+
+        [Property(Arbitrary = new[] { typeof(TemperatureMeasurementGenerator) })]
+        public void TestAssociativity(decimal v1, decimal v2, decimal v3)
+        {
+            var ts1 = TimeSeriesEntries.FromMeasurement(v1);
+            var ts2 = TimeSeriesEntries.FromMeasurement(v2);
+            var ts3 = TimeSeriesEntries.FromMeasurement(v3);
+
+            var r1 = TimeSeriesEntries.Combine(ts1, TimeSeriesEntries.Combine(ts2, ts3));
+            var r2 = TimeSeriesEntries.Combine(TimeSeriesEntries.Combine(ts1, ts2), ts3);
+
+            Assert.Equal(r1, r2);
+        }
     }
-
-
-
 }
